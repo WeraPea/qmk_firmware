9a464c2d71bfad9e9a1e26c3689ef7cd28093e17
WeraPea <sokneip@tuta.io>
2024-04-27 20:26:45 +0200

    cleaned up the keymap file

--- keyboards/splitkb/kyria/keymaps/WeraPea/keymap.c
+++ keyboards/splitkb/kyria/keymaps/WeraPea/keymap.c
@@ -99,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = LAYOUT(
      KC_TAB  , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                                     KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
-	 KC_LGUI , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                                     KC_SCLN, KC_H   , KC_J   , KC_K   , KC_L   ,CTL_QUOT,
+	 KC_LGUI , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                                     KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN,CTL_QUOT,
      KC_LSFT , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_MSEL, KC_SYRQ, NUM    , KC_MSEL, KC_N   , KC_M   , KC_COMM, KC_DOT ,ALT_SLSH, KC_RSFT,
                                  GUI_NUM, KC_LCTL, ALT_SPC, ENT_SYM, QK_LEAD, ENT_SYM, NAV_SPC,LALT_BKSP,KC_LGUI, MOUSE_TILD
     ),
@@ -140,9 +140,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_NAV] = LAYOUT(
-      _______, _______, _______, _______, _______, _______,                                     KC_VOLU, KC_PGUP, KC_PAUS, KC_HOME, KC_END , KC_DEL ,
-      _______, _______, _______, _______, _______, _______,                                     KC_VOLD, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_INS ,
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_PGDN, KC_MPRV, KC_MPLY, KC_MNXT, KC_PSCR,
+      _______, _______, _______, _______, _______, _______,                                     KC_PGUP, KC_PAUS, KC_HOME, KC_END , KC_VOLU, KC_DEL ,
+      _______, _______, _______, _______, _______, _______,                                     KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_VOLD, KC_INS ,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGDN, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_PSCR,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
@@ -210,9 +210,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
 	[_MOUSE] = LAYOUT(
-	  _______, _______, _______, _______, _______, _______,                                     _______, KC_WH_U, KC_BTN1, KC_BTN3, KC_BTN2, _______,
-	  _______, _______, _______, _______, _______, _______,                                     _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______,
-	  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_WH_D, KC_ACL0, KC_ACL1, KC_ACL2, _______,
+	  _______, _______, _______, _______, _______, _______,                                     KC_WH_U, KC_BTN1, KC_BTN3, KC_BTN2, _______, _______,
+	  _______, _______, _______, _______, _______, _______,                                     KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
+	  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_WH_D, KC_ACL0, KC_ACL1, KC_ACL2, _______, _______,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 	),
 
